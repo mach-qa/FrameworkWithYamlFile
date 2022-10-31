228b2c60c112f87798ab6793e7bf9a33e9f12db9
mach-qa <56649147+mach-qa@users.noreply.github.com>
2022-10-31 22:03:13 +0100

    Update Homework

--- src/main/java/models/Config.java
+++ src/main/java/models/Config.java
@@ -1,33 +1,12 @@
 package models;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 
 @Data
 @NoArgsConstructor
 public class Config {
-
-//    protected String activeEnvironment;
-//    protected Browser browser;
-//
-//    private Map<String, Environments> environment = new LinkedHashMap<>();
-//
-//    @JsonAnyGetter
-//    void setEnvironment(String key, Environments value) {
-//        environment.put(key, value);
-//    }
-//
-//    @JsonAnySetter
-//    public Map<String, Environments> getEnvironment() {
-//        return environment;
-//    }
 
     public String activeEnvironment;
     public Environments environment;
